1f4cc935b9955b8981e7e87459cd9352d8150d3d
baalaji03 <rps123a@gmail.com>
2024-06-21 16:28:01 -0700

    updated

--- index.js
+++ index.js
@@ -13,6 +13,11 @@ app.get('/',(req,res)=>{
    //res.status(200).json({message:"hii all welcome to our first node app"})
 })
 
+app.get('/data',(res,req)=>{
+   res.status(200).send(`<span style="background: linear-gradient(to left, #33ccff 0%, #ff99cc 100%); color:black;font-size:100px">hii welcome to node project Day-1</span>`)
+
+})
+
 
 app.listen(port,()=>{
    console.log(`App is a listening on the port ${port}`)
