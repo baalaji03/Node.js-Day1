8d7f0274f1a77fc5696dccb2777165bd376ff13d
baalaji03 <rps123a@gmail.com>
2024-06-21 16:14:51 -0700

    completed

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from "express";
+
+
+const app=express();
+const port=4000;
+
+//middleware
+app.use(express.json())
+
+//routes
+app.get('/',(req,res)=>{
+   res.status(200).send(`<span style="background: linear-gradient(to left, #33ccff 0%, #ff99cc 100%); color:black;font-size:100px">hii welcome to node project</span>`)
+   //res.status(200).json({message:"hii all welcome to our first node app"})
+})
+
+
+app.listen(port,()=>{
+   console.log(`App is a listening on the port ${port}`)
+})
